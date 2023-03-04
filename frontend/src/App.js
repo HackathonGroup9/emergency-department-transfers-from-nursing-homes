e88ac221a04ab1a97bcd72fae1ab167c5f765aed
ametwali <104875958+ametwali@users.noreply.github.com>
2023-03-04 10:20:54 -0700

    added router stuff, to install it do "npm i react-router-dom"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import{BrowserRouter, Routes,Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element/>
+        </Routes>
+      </BrowserRouter>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +25,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
