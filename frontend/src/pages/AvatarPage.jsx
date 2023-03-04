d65d2adc494b185ccfd092f2cf467afa22743df5
VladT7 <vladt13@gmail.com>
2023-03-04 11:44:15 -0700

    add multi-seelct drop down menu

--- frontend/src/pages/AvatarPage.jsx
+++ frontend/src/pages/AvatarPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Avatar from "../components/Avatar";
+import SignsSymptoms from "../components/SignsSymptoms";
 
 function AvatarPage() {
   return (
       <page>
         <Avatar></Avatar>
+        <SignsSymptoms></SignsSymptoms>
       </page>
   );
 }
