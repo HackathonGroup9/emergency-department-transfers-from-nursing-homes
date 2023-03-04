fb5fdb6979c422c202aa733ab4ed4c271dbabc4e
ametwali <104875958+ametwali@users.noreply.github.com>
2023-03-04 10:55:06 -0700

    Update PatientHomePage.jsx

--- frontend/src/pages/PatientHomePage.jsx
+++ frontend/src/pages/PatientHomePage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
-
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 
 export const PatientHomePage = () => {
+const [patient, savepatient] = useState([]) 
+
+useEffect(() => {
+    const fetchpatients = async () => { //making an api request with the async
+        try{
+            const res = await axios.get("http:localhost:3000/patient")
+            console.log (res)
+        }catch(err){
+            console.log(err)
+        }
+    }
+})
+
   return (
     <div>
         PatientHomePage
