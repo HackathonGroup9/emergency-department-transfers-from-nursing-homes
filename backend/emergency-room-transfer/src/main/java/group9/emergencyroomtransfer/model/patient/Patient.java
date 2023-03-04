b552e8552286dcd41befa6677fef409e8ea074d6
Jessica Nguyen <jessica_nguyen09@live.ca>
2023-03-04 11:16:42 -0700

    add select patient controller

--- backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/model/patient/Patient.java
+++ backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/model/patient/Patient.java
@@ -5,9 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Table(name="patient")
 public class Patient {
 
