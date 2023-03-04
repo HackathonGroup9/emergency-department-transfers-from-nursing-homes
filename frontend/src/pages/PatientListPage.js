447ca38522dbc175862f9b4b7a4ff095c79cf5fa
Trevor Le <trevor.le1@ucalgary.ca>
2023-03-04 10:17:17 -0700

    Rendered PatientList

---
+++ frontend/src/pages/PatientListPage.js
@@ -0,0 +1,11 @@
+import PatientList from "../components/PatientList";
+
+function PatientListPage() {
+  return (
+    <div className="PatientListPage">
+      <PatientList />
+    </div>
+  );
+}
+
+export default PatientListPage;
