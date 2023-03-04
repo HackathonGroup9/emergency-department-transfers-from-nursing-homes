3e59eacb02f455adb102dd662581f24cb11f752e
Jessica Nguyen <jessica_nguyen09@live.ca>
2023-03-04 09:39:18 -0700

    Initialize backend

---
+++ backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/EmergencyRoomTransferApplication.java
@@ -0,0 +1,13 @@
+package group9.emergencyroomtransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyRoomTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyRoomTransferApplication.class, args);
+	}
+
+}
---
+++ backend/emergency-room-transfer/src/test/java/group9/emergencyroomtransfer/EmergencyRoomTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package group9.emergencyroomtransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyRoomTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
