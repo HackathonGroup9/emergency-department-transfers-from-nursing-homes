35f684bcaeb99b369c573a0476fae378703cd795
rickngo <rick.ngo@hotmail.com>
2023-03-04 11:51:35 -0700

    Update database

--- backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/model/patient/Patient.java
+++ backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/model/patient/Patient.java
@@ -1,27 +1,25 @@
 package group9.emergencyroomtransfer.model.patient;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.io.Serializable;
 
 @Entity
 @Getter
 @Setter
 @Table(name="patient")
-public class Patient {
+public class Patient implements Serializable {
 
     @Id
     @Column(name = "phn")
-    private Long pcn;
+    private Long phn;
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-    @Column(name = "dob")
-    private String dob;
+    @Column(name = "birth_date")
+    private String birthDate;
 
 }
