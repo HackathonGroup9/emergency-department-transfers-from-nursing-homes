014ace78152d5395e996be0738598ff2886fb1a4
rickngo <rick.ngo@hotmail.com>
2023-03-04 12:21:03 -0700

    Add System entity, controller, service, repo

--- backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/model/system/System.java
+++ backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/model/system/System.java
@@ -1,4 +1,22 @@
 package group9.emergencyroomtransfer.model.system;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "biosystem")
 public class System {
+
+    @Id
+    @Column(name = "system_id")
+    private int systemId;
+    @Column(name = "system_name")
+    private String systemName;
+
 }
