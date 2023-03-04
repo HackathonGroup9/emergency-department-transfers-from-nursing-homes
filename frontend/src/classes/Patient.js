270400b80a27281286c504acda49a44b46a2998b
ametwali <104875958+ametwali@users.noreply.github.com>
2023-03-04 13:52:59 -0700

    fixed api

---
+++ frontend/src/classes/Patient.js
@@ -0,0 +1,8 @@
+export default class Patient{
+    constructor(phn, firstName, lastName, birthDate){
+        this.phn = phn;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+}
