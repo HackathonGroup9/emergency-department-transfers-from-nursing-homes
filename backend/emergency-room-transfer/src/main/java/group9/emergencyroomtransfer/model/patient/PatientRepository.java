601278d6150d29d043ca23773f2cd3dbc4aa69a1
rickngo <rick.ngo@hotmail.com>
2023-03-04 10:34:28 -0700

    Setup patient

--- backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/model/patient/PatientRepository.java
+++ backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/model/patient/PatientRepository.java
@@ -1,4 +1,8 @@
 package group9.emergencyroomtransfer.model.patient;
 
-public class PatientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
 }
