f766e813cd41a4b4b81963e0c87c0e38fd495327
rickngo <rickngo@users.noreply.github.com>
2023-03-04 12:21:32 -0700

    Add System entity, controller, service, repo (#7)

--- backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/controller/SelectSystemController.java
+++ backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/controller/SelectSystemController.java
@@ -1,4 +1,28 @@
 package group9.emergencyroomtransfer.controller;
 
+import group9.emergencyroomtransfer.model.system.System;
+import group9.emergencyroomtransfer.service.SelectSystemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/systems")
 public class SelectSystemController {
+
+    private final SelectSystemService selectSystemService;
+
+    @Autowired
+    public SelectSystemController(SelectSystemService selectSystemService) {
+        this.selectSystemService = selectSystemService;
+    }
+
+    @GetMapping
+    public List<System> getAllSystems() {
+        return selectSystemService.getAll();
+    }
+
 }
--- backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/service/SelectPatientService.java
+++ backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/service/SelectPatientService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Service
 public class SelectPatientService {
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
 
     @Autowired
     public SelectPatientService(PatientRepository patientRepository) {
