8ae02d755f7939ec31de3af79870eea00403e8ff
rickngo <rickngo@users.noreply.github.com>
2023-03-04 10:34:49 -0700

    Setup patient (#3)

--- backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/model/patient/Patient.java
+++ backend/emergency-room-transfer/src/main/java/group9/emergencyroomtransfer/model/patient/Patient.java
@@ -1,4 +1,24 @@
 package group9.emergencyroomtransfer.model.patient;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="patient")
 public class Patient {
+
+    @Id
+    @Column(name = "phn")
+    private Long pcn;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "dob")
+    private String dob;
+
 }
