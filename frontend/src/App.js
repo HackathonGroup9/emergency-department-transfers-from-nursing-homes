9e6bea69a5718fa025afcdf5cd6400e99fd89aac
VladT7 <vladt13@gmail.com>
2023-03-04 11:51:14 -0700

    add static buttons to every page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PatientHomePage } from "./pages/PatientHomePage";
 // import { Avatar } from "@mui/material";/
 import AvatarPage from "./pages/AvatarPage";
+import StaticButtons from "./components/StaticButtons";
 
 export default function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<PatientListPage />} />
-          <Route path="/avatarpage" element={<AvatarPage/>} />
-          <Route path="/patienthomepage" element={<PatientHomePage />} />
-
+          <Route
+            path="/"
+            element={<PatientListPage />}
+          />
+          <Route
+            path="/avatarpage"
+            element={<AvatarPage />}
+          />
+          <Route
+            path="/patienthomepage"
+            element={<PatientHomePage />}
+          />
         </Routes>
+
+        <StaticButtons></StaticButtons>
       </BrowserRouter>
     </div>
   );
---
+++ frontend/src/components/StaticButtons.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+
+function StaticButtons() {
+  return (
+    <div>
+      <Button>Physician Communication Binder</Button>
+      <Button>Physician and Alternative Communication Numbers</Button>
+      <Button>Return to Unit-Specific Patients List</Button>
+      <Button>LOGOUT</Button>
+    </div>
+  );
+}
+
+export default StaticButtons;
