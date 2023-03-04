788d0e8f9a60dda2299b43be81f6b53a932bb556
VladT7 <vladt13@gmail.com>
2023-03-04 12:01:12 -0700

    link patient homepage to avatarpage

--- frontend/src/components/StaticButtons.jsx
+++ frontend/src/components/StaticButtons.jsx
@@ -3,7 +3,7 @@ import { Button } from "semantic-ui-react";
 
 function StaticButtons() {
   return (
-    <div>
+    <div >
       <Button>Physician Communication Binder</Button>
       <Button>Physician and Alternative Communication Numbers</Button>
       <Button>Return to Unit-Specific Patients List</Button>
