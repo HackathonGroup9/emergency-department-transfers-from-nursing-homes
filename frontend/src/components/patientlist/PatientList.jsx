7bfcfbd633a77fa0eb1804d332010b609ff5eb07
Trevor Le <trevor.le1@ucalgary.ca>
2023-03-04 14:32:05 -0700

    Fixed patient list api call

--- frontend/src/components/patientlist/PatientList.jsx
+++ frontend/src/components/patientlist/PatientList.jsx
@@ -1,43 +1,26 @@
-import { useState } from "react";
 import { Button, Dropdown } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import "./PatientList.css";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
 
 function PatientList() {
   const [patient, setPatient] = useState("");
 
-  const patients = [
-    {
-      key: "Jenny Hess",
-      text: "Jenny Hess",
-      value: "1234",
-    },
-    {
-      key: "Elliot Fu",
-      text: "Elliot Fu",
-      value: "4321",
-    },
-    {
-      key: "Stevie Feliciano",
-      text: "Stevie Feliciano",
-      value: "4444",
-    },
-    {
-      key: "Christian",
-      text: "Christian",
-      value: "2222",
-    },
-    {
-      key: "Matt",
-      text: "Matt",
-      value: "1111",
-    },
-    {
-      key: "Justen Kitsune",
-      text: "Justen Kitsune",
-      value: "9999",
-    },
-  ];
+  const [patientList, setPatientList] = useState([]);
+
+  const fetchPatients = useCallback(async () => {
+    await axios
+      .get("http://localhost:8080/api/patients")
+      .then((response) => {
+        setPatientList(response.data);
+      })
+      .catch((error) => console.log(error.response.data));
+  }, []);
+
+  useEffect(() => {
+    fetchPatients();
+  }, [fetchPatients]);
 
   const getPatient = (e, value) => {
     console.log(value);
@@ -49,7 +32,13 @@ function PatientList() {
       <Dropdown
         placeholder="Select Patient"
         selection
-        options={patients}
+        options={patientList.map((patient) => {
+          return {
+            key: patient.phn,
+            text: patient.firstName + " " + patient.lastName,
+            value: patient.phn,
+          };
+        })}
         onChange={(event, { value }) => getPatient(event, value)}
       />
       <Link
