7f213f24264927857837642e900abab3990529f1
Trevor Le <trevor.le1@ucalgary.ca>
2023-03-04 10:36:48 -0700

    Integrated PatientListPage to router

--- frontend/src/pages/PatientListPage.js
+++ frontend/src/pages/PatientListPage.js
@@ -3,7 +3,7 @@ import PatientList from "../components/PatientList";
 function PatientListPage() {
   return (
     <div className="PatientListPage">
-      <PatientList />
+      <p>Test</p>
     </div>
   );
 }
